41899b80a8b296633e145c1fd00350f089da0f2f
bumpMind <damian.s.ja@gmail.com>
2023-07-11 21:04:20 +0200

    ci: update build script comments

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,19 @@
+# Install node
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+apt install nodejs
+
+# Create client app build
 npm install
 npm run build
 
+#Setup ngnix
 apt install nginx
+# TODO pdate ngnix config
 
+# Copy client app build to the correct place (switch to the S3)
 mkdir /var/www/
 scp -r ./build/* /var/www/build/
 
-# Update ngnix config
-
+# Start ngnix service
 service nginx stop
 service nginx start
